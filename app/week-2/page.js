ef6dd9d5d0c6cabb0162af23252d6f1ea0bc637d
Parmeetbhullar <parmeetprofessional@gmail.com>
2025-01-23 09:56:02 -0700

    Assignment

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,9 @@
+import StudentInfo from "./student-info";
 export default function Page() {
     return (
-      <main>
+      <div>
         <h1>Shopping List</h1>
-      </main>
+        <StudentInfo />
+      </div>
     );
   }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
-export default function StudentInfo () {
-return (
-    <main>
-            <p>Parmeet Singh</p>
-        <p> <Link href="https://github.com/Parmeetbhullar" > My Github Link </Link> </p> 
-        <p> <Link href="/" > Go back </Link> </p> 
 
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Parmeet Singh</p>
+      <p>
+        <Link href="https://github.com/Parmeetbhullar/cprg306-assignments.git" >https://github.com</Link>
+      </p>
     </main>
-);
-}   
+  );
+}
