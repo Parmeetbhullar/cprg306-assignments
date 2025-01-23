019f8993f6f6bcf7d8e1907dd207bc509ceba29e
Parmeetbhullar <parmeetprofessional@gmail.com>
2025-01-23 09:28:32 -0700

    Initial commit from create-next-app

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function StudentInfo () {
+return (
+    <main>
+            <p>Parmeet Singh</p>
+        <p> <Link href="https://github.com/Parmeetbhullar" > My Github Link </Link> </p> 
+        <p> <Link href="/" > Go back </Link> </p> 
+
+    </main>
+);
+}   
