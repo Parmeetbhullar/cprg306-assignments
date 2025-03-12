0302660ae74180485355e3dbab58094a19521438
Parmeetbhullar <parmeetprofessional@gmail.com>
2025-03-11 23:26:48 -0600

    week-7

---
+++ app/week-7/page.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import NewItem from "./new-item";
+import ItemList from "./item-list";
+import itemsData from "./items.json";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  function handleAddItem(newItem) {
+    setItems((prevItems) => [...prevItems, newItem]);
+  }
+
+  return (
+    <main className="p-5 bg-gray-900 min-h-screen text-white">
+      <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
+    </main>
+  );
+}
